f98b8acb395de4021a3e552f2beae776cff7878c
Sean Lin <30335635+Seanlinsanity@users.noreply.github.com>
2023-06-28 15:37:25 +0800

    Subtree Of Another Tree

---
+++ src/com/seanlindev/algorithms/SubtreeOfAnotherTree.java
@@ -0,0 +1,63 @@
+package com.seanlindev.algorithms;
+
+import com.seanlindev.utils.TreeNode;
+
+/*
+Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+
+A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+
+
+
+Example 1:
+
+
+Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+Output: true
+Example 2:
+
+
+Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+Output: false
+
+
+Constraints:
+
+The number of nodes in the root tree is in the range [1, 2000].
+The number of nodes in the subRoot tree is in the range [1, 1000].
+-104 <= root.val <= 104
+-104 <= subRoot.val <= 104
+ */
+public class SubtreeOfAnotherTree {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) {
+            return true;
+        }
+
+        if (root == null || subRoot == null) {
+            return false;
+        }
+
+        if (sameTree(root, subRoot)) {
+            return true;
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean sameTree(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+
+        if (node1.val != node2.val) {
+            return false;
+        }
+
+        return sameTree(node1.left, node2.left) && sameTree(node1.right, node2.right);
+    }
+}
