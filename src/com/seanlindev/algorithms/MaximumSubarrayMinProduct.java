1b82186eeedd80d3e6b3e81e95a3005219746acb
Seanlinsanity <seanlin1016@gmail.com>
2023-08-22 11:21:36 +0800

    Maximum Subarray Min-Product

---
+++ src/com/seanlindev/algorithms/MaximumSubarrayMinProduct.java
@@ -0,0 +1,71 @@
+package com.seanlindev.algorithms;
+
+import java.util.Stack;
+
+/*
+The min-product of an array is equal to the minimum value in the array multiplied by the array's sum.
+
+For example, the array [3,2,5] (minimum value is 2) has a min-product of 2 * (3+2+5) = 2 * 10 = 20.
+Given an array of integers nums, return the maximum min-product of any non-empty subarray of nums. Since the answer may be large, return it modulo 109 + 7.
+
+Note that the min-product should be maximized before performing the modulo operation. Testcases are generated such that the maximum min-product without modulo will fit in a 64-bit signed integer.
+
+A subarray is a contiguous part of an array.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,2]
+Output: 14
+Explanation: The maximum min-product is achieved with the subarray [2,3,2] (minimum value is 2).
+2 * (2+3+2) = 2 * 7 = 14.
+Example 2:
+
+Input: nums = [2,3,3,1,2]
+Output: 18
+Explanation: The maximum min-product is achieved with the subarray [3,3] (minimum value is 3).
+3 * (3+3) = 3 * 6 = 18.
+Example 3:
+
+Input: nums = [3,1,5,6,4,2]
+Output: 60
+Explanation: The maximum min-product is achieved with the subarray [5,6,4] (minimum value is 4).
+4 * (5+6+4) = 4 * 15 = 60.
+
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 107
+ */
+public class MaximumSubarrayMinProduct {
+    public int maxSumMinProduct(int[] nums) {
+        long[] prefix = new long[nums.length + 1];
+        for (int i = 1; i < nums.length + 1; i++) {
+            prefix[i] = (prefix[i - 1] + nums[i - 1]);
+        }
+
+        long result = 0;
+        Stack<long[]> stack = new Stack<>();
+        for (int i = 0; i < nums.length; i++) {
+            long startIndex = i;
+            while (!stack.isEmpty() && stack.peek()[1] > nums[i]) {
+                long[] node = stack.pop();
+                long sum = prefix[i] - prefix[(int)node[0]];
+                result = Math.max(result, sum * node[1]);
+                startIndex = node[0];
+            }
+            stack.push(new long[]{startIndex, nums[i]});
+        }
+
+        while (!stack.isEmpty()) {
+            long[] node = stack.pop();
+            long sum = prefix[nums.length] - prefix[(int)node[0]];
+            result = Math.max(result, sum * node[1]);
+        }
+
+        result = result % (long)(Math.pow(10, 9) + 7);
+        return (int)result;
+    }
+}
