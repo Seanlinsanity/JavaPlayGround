8946263fc96d1edee7ee56893a18bfcc18221202
Sean Lin <seanlin.dev@gmail.com>
2024-09-01 15:51:29 +0800

    H Index

---
+++ src/com/seanlindev/algorithms/HIndex.java
@@ -0,0 +1,46 @@
+package com.seanlindev.algorithms;
+
+/*
+Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
+
+
+
+Example 1:
+
+Input: citations = [3,0,6,1,5]
+Output: 3
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+Example 2:
+
+Input: citations = [1,3,1]
+Output: 1
+
+
+Constraints:
+
+n == citations.length
+1 <= n <= 5000
+0 <= citations[i] <= 1000
+ */
+public class HIndex {
+    public int hIndex(int[] citations) {
+        int[] citationCountArray = new int[citations.length + 1];
+        for (int citation: citations) {
+            int citationIndex = Math.min(citation, citations.length);
+            citationCountArray[citationIndex] += 1;
+        }
+
+        int total = 0;
+        for (int h = citations.length; h >= 0; h--) {
+            total += citationCountArray[h];
+            if (total >= h) {
+                return h;
+            }
+        }
+
+        return 0;
+    }
+}
