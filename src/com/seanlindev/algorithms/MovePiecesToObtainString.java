5f93fb71205be107e873b34d59c50cbd9859de03
Seanlinsanity <seanlin.dev@gmail.com>
2025-05-12 08:22:09 +0800

    Move Pieces to Obtain a String

---
+++ src/com/seanlindev/algorithms/MovePiecesToObtainString.java
@@ -0,0 +1,104 @@
+package com.seanlindev.algorithms;
+
+/*
+You are given two strings start and target, both of length n. Each string consists only of the characters 'L', 'R', and '_' where:
+
+The characters 'L' and 'R' represent pieces, where a piece 'L' can move to the left only if there is a blank space directly to its left, and a piece 'R' can move to the right only if there is a blank space directly to its right.
+The character '_' represents a blank space that can be occupied by any of the 'L' or 'R' pieces.
+Return true if it is possible to obtain the string target by moving the pieces of the string start any number of times. Otherwise, return false.
+
+
+
+Example 1:
+
+Input: start = "_L__R__R_", target = "L______RR"
+Output: true
+Explanation: We can obtain the string target from start by doing the following moves:
+- Move the first piece one step to the left, start becomes equal to "L___R__R_".
+- Move the last piece one step to the right, start becomes equal to "L___R___R".
+- Move the second piece three steps to the right, start becomes equal to "L______RR".
+Since it is possible to get the string target from start, we return true.
+Example 2:
+
+Input: start = "R_L_", target = "__LR"
+Output: false
+Explanation: The 'R' piece in the string start can move one step to the right to obtain "_RL_".
+After that, no pieces can move anymore, so it is impossible to obtain the string target from start.
+Example 3:
+
+Input: start = "_R", target = "R_"
+Output: false
+Explanation: The piece in the string start can move only to the right, so it is impossible to obtain the string target from start.
+
+
+Constraints:
+
+n == start.length == target.length
+1 <= n <= 105
+start and target consist of the characters 'L', 'R', and '_'.
+ */
+public class MovePiecesToObtainString {
+    public boolean canChange(String start, String target) {
+        if (start.length() != target.length()) { return false; }
+        int length = start.length();
+        int index1 = 0; //index1=0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8
+        int index2 = 0; //index2=0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9
+        while (index1 < length && index2 < length) {
+            char char1 = start.charAt(index1); //char1=_->L->_->_->R->_->_->R
+            if (char1 == '_') {
+                index1 += 1;
+                continue;
+            }
+
+            if (char1 == 'L') {
+                while (index2 < length) {
+                    if (target.charAt(index2) == '_') {
+                        index2 += 1;
+                    } else if (target.charAt(index2) == 'R') {
+                        return false;
+                    } else {
+                        if (index1 < index2) {
+                            return false;
+                        }
+                        index2 += 1;
+                        index1 += 1;
+                        break;
+                    }
+                }
+            } else if (char1 == 'R') {
+                while (index2 < length) {
+                    if (target.charAt(index2) == '_') {
+                        index2 += 1;
+                    } else if (target.charAt(index2) == 'L') {
+                        return false;
+                    } else {
+                        if (index1 > index2) {
+                            return false;
+                        }
+                        index2 += 1;
+                        index1 += 1;
+                        break;
+                    }
+                }
+            }
+        }
+
+        while (index1 < length) {
+            if (start.charAt(index1) == '_') {
+                index1 += 1;
+            } else {
+                return false;
+            }
+        }
+
+        while (index2 < length) {
+            if (target.charAt(index2) == '_') {
+                index2 += 1;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
